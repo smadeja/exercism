c309fdc5e913dd219772b714a1abf69880386d92
Szymon Madeja <smadeja1@gmail.com>
2019-02-24 18:19:24 +0100

    Simplify the logic by using a comprehension

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -7,7 +7,7 @@ defmodule Strain do
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def keep(list, fun) do
-    list |> do_keep(fun)
+    for x <- list, fun.(x), do: x
   end
 
   @doc """
@@ -20,22 +20,4 @@ defmodule Strain do
   def discard(list, fun) do
     list |> keep(&(!fun.(&1)))
   end
-
-  @spec do_keep(
-          list :: list(any),
-          function :: (any -> boolean),
-          accumulator :: list(any)
-        ) ::
-          list(any)
-  defp do_keep(list, function, accumulator \\ [])
-
-  defp do_keep([head | tail], function, accumulator) do
-    if function.(head) do
-      tail |> do_keep(function, [head | accumulator])
-    else
-      tail |> do_keep(function, accumulator)
-    end
-  end
-
-  defp do_keep([], _function, accumulator), do: accumulator |> :lists.reverse()
 end
